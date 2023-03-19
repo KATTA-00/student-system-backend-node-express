d7cf0a7ceed171f8f1befc7bb1e120320269f959
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-03-19 22:41:04 +0530

    mongo setup

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
+const connectDB = require("./config/connectDB");
 const register = require("./routes/registration");
-const students = require("./routes/students");
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/register", register);
-// app.use("/students", students);
 
-app.listen(PORT, () => {
-  console.log(`Server is up and running at port ${PORT}`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
--- routes/registration.js
+++ routes/registration.js
@@ -1,67 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Student = require("../model/Student");
 
-const data = [
-  {
-    id: 1,
-    firstname: "Kanishka",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-  {
-    id: 2,
-    firstname: "Kanishka",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-  {
-    id: 3,
-    firstname: "Kanishka",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-  {
-    id: 4,
-    firstname: "Kanishka",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-  {
-    id: 5,
-    firstname: "Kanishka",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-  {
-    id: 6,
-    firstname: "Kanishka",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-  {
-    id: 7,
-    firstname: "Hi",
-    lastname: "Gunawardana",
-    Eno: "E/19/129",
-    email: "kanishkagunawardana@gmail.com",
-  },
-];
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const data = await Student.find();
   res.status(200).json(data);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const temp = { ...req.body };
-  temp.id = data[data.length - 1].id + 1;
-  data.push(temp);
-  console.log(req.body);
+  // temp.id = data[data.length - 1].id + 1;
+  // data.push(temp);
+  // console.log(req.body);
+
+  const result = await Student.create(temp);
+
   res.status(200).send("hii");
 });
 
